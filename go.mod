e6387f3383c347c47522fb9118988fbbb0f00ad2
Iain Lumsden <iain.a.lumsden@gmail.com>
2020-06-28 12:38:55 +1200

    added random moving servo

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/Malachiain/camera-control
 
 go 1.14
+
+require (
+	gobot.io/x/gobot v1.14.0
+)
---
+++ servo-control.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"gobot.io/x/gobot"
+	"gobot.io/x/gobot/drivers/gpio"
+	"gobot.io/x/gobot/platforms/raspi"
+)
+
+func main() {
+	r := raspi.NewAdaptor()
+	servo := gpio.NewServoDriver(r, "18")
+
+	work := func() {
+		gobot.Every(1*time.Second, func() {
+			i := uint8(gobot.Rand(180))
+			fmt.Println("Turning", i)
+			servo.Move(i)
+		})
+	}
+
+	robot := gobot.NewRobot("servoBot",
+		[]gobot.Connection{r},
+		[]gobot.Device{servo},
+		work,
+	)
+
+	robot.Start()
+}
